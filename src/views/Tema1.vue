e79e3d5eecdedbe6d9779bfc70fcf9d5ed5e8b2e
Andrea Botello <andrea.botellor@misena.edu.co>
2023-04-14 14:48:57 -0500

    Ajustes

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -50,7 +50,7 @@
     .row.mb-5(data-aos="fade-down")
       figure
         .video
-          iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+          iframe(width="560" height="315" src="https://www.youtube.com/embed/W5y9tAn4w0Y" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
 
     .row.mb-3
       p Teniendo claro qué es una matriz de requisitos legales o matriz legal, a continuación, se da cuenta de cuál es la normatividad para crearla:
